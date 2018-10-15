72855fe7631650e986aa292c305fe898d92e6e98
Vladimir Gonikhin <vgonikhin@gmail.com>
2018-10-15 23:35:13 +0300

    Initial commit

--- app/src/main/java/ru/vgonikhin/aa/newsapp/MainActivity.java
+++ app/src/main/java/ru/vgonikhin/aa/newsapp/MainActivity.java
@@ -1,13 +1,58 @@
 package ru.vgonikhin.aa.newsapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button buttonSend;
+    ImageButton buttonGithub;
+    ImageButton buttonTelegram;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.edittext_message);
+        buttonSend = findViewById(R.id.button_send);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                composeEmail("test@email.com", editText.getText().toString());
+            }
+        });
+        buttonGithub = findViewById(R.id.button_github);
+        buttonGithub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://github.com/username")));
+            }
+        });
+        buttonTelegram = findViewById(R.id.button_telegram);
+        buttonTelegram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://t.me/username")));
+            }
+        });
+    }
+
+    public void composeEmail(String address, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.putExtra(Intent.EXTRA_EMAIL, address);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(),getString(R.string.error_no_email),Toast.LENGTH_LONG).show();
+        }
     }
 }
