5f1b6d7a73b749c3c92605644409597200cadfa4
Vladimir Gonikhin <vgonikhin@gmail.com>
2018-10-17 11:50:42 +0300

    1. Optimized methods in AboutActivity 2. Added ButterKnife

--- app/src/main/java/ru/vgonikhin/aa/newsapp/AboutActivity.java
+++ app/src/main/java/ru/vgonikhin/aa/newsapp/AboutActivity.java
@@ -10,49 +10,66 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 public class AboutActivity extends AppCompatActivity {
 
-    EditText editText;
-    Button buttonSend;
-    ImageButton buttonGithub;
-    ImageButton buttonTelegram;
+    @BindView(R.id.edittext_message) EditText editText;
+    @BindView(R.id.button_send) Button buttonSend;
+    @BindView(R.id.button_github) ImageButton buttonGithub;
+    @BindView(R.id.button_telegram) ImageButton buttonTelegram;
+
+    Unbinder unbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        editText = findViewById(R.id.edittext_message);
-        buttonSend = findViewById(R.id.button_send);
-        buttonSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                composeEmail("test@email.com", editText.getText().toString());
-            }
-        });
-        buttonGithub = findViewById(R.id.button_github);
-        buttonGithub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://github.com/username")));
-            }
-        });
-        buttonTelegram = findViewById(R.id.button_telegram);
-        buttonTelegram.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://t.me/username")));
-            }
-        });
+        unbinder = ButterKnife.bind(this);
     }
 
-    public void composeEmail(String address, String message) {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.putExtra(Intent.EXTRA_EMAIL, address);
-        intent.putExtra(Intent.EXTRA_TEXT, message);
+    @OnClick(R.id.button_send)
+    public void sendFeedback(){
+        composeEmail(editText.getText().toString());
+    }
+
+    @OnClick(R.id.button_github)
+    public void contactGithub(){
+        viewLink(getString(R.string.github_link));
+    }
+
+    @OnClick(R.id.button_telegram)
+    public void contactTelegram(){
+        viewLink(getString(R.string.telegram_link));
+    }
+
+    public void viewLink(String link){
+        Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(link));
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         } else {
-            Toast.makeText(getApplicationContext(),getString(R.string.error_no_email),Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(),getString(R.string.error_no_app),Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void composeEmail(String text) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO)
+                .setData(Uri.parse(getString(R.string.mailto_address)))
+                .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject))
+                .putExtra(Intent.EXTRA_TEXT, text);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(),getString(R.string.error_no_app),Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbinder.unbind();
+        super.onDestroy();
     }
 }
